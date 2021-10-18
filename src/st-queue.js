bd67114a6360f92d91dd60dc0dca01783167bf1e
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-10-18 22:23:39 +0300

    feat: complete task- (ST) Queue (Mon, Oct 18, 2021 10:23:40 PM)

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,33 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
 module.exports = class Queue {
+    constructor() {
+        this.head = null
+        this.last = null
+    }
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getUnderlyingList() {
+        return this.head
+    }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    enqueue(value) {
+      let node = new ListNode(value)
+      if(!this.head){
+        this.head = node
+        this.last = this.head
+      } else{
+        this.last.next = node
+        this.last = this.last.next
+      }
+    }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    dequeue() {
+      let result = this.head.value
+      this.head = this.head.next
+      if (this.head.next !== null){
+        this.last = null
+      }
+      return result
+    }
 
+}
+
+function ListNode(x, nextX = null) {
+  this.value = x
+  this.next = nextX
 }
