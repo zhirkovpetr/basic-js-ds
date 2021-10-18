2c787f7e4adcf6dbc1e4eabc88e3e814aec30d19
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-10-18 21:34:33 +0300

    feat: complete task- (ST) Stack (Mon, Oct 18, 2021 09:34:24 PM)

--- src/st-stack.js
+++ src/st-stack.js
@@ -1,30 +1,15 @@
-const { NotImplementedError } = require('../extensions/index.js');
+module.exports = class Stack {
+    stack = []
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(element) {
+        return this.stack.push(element)
+    }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+      return this.stack.pop()
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    peek() {
+      return this.stack[this.stack.length - 1]
+    }
 }
